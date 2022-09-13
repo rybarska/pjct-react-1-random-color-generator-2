9aef3da2707358a3d9c633eaa105fbfb1ec21ac9
agata <rybarska>
2022-09-13 09:34:05 +0200

    Add libraries

--- src/App.js
+++ src/App.js
@@ -3,36 +3,6 @@ import { GetColorName } from 'hex-color-to-color-name';
 import { randomColor } from 'randomcolor';
 import { useState } from 'react';
 
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
-
-@rybarska
-@rybarska
-rybarska
-/
-pjct-react-1-random-color-generator
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-0
-Settings
-More
-pjct-react-1-random-color-generator/src/App.js /
-
-agata Add uppercase on first color
-Latest commit e66dbaf 15 hours ago
- History
- 0 contributors
-41 lines (39 sloc)  975 Bytes
-
 function App() {
   const [hexCode, setHexCode] = useState('#ff0000');
   const [colorName, setColorName] = useState('Red');
@@ -69,18 +39,3 @@ function App() {
 }
 
 export default App;
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-You have unread notifications
